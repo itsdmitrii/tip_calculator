bddb31b9340b0134b44c77b72cd9601855cb8b21
Dmitrii Dorogov <dmitri.dorogov@gmail.com>
2022-06-10 23:50:11 -0700

    Add files via upload

---
+++ tip_calculator.py
@@ -0,0 +1,21 @@
+print("Welcome to the tip calculator!\n")
+
+total_bill = input("What was the total bill? $\n")
+
+tip_percentage = input("What petcentage tip would you like to give? 10, 12, or 15%?\n")
+
+total_people_split = input ("How many people to split the bill? \n")
+
+total_bill_as_int = int(total_bill)
+tip_percentage_as_int = int(tip_percentage)
+total_people_split_int = int(total_people_split)
+
+# Calculations:
+percentage_with_tip = 100 + tip_percentage_as_int
+
+total_with_tip = total_bill_as_int * percentage_with_tip / 100
+
+each_person_pay = total_with_tip / total_people_split_int
+
+
+print(f"Each person shoud pay: {each_person_pay} $")
